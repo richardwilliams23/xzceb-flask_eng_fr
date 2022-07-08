1a180386cb6ccea3d495bdd77dc85f5638911d85
richardwilliams23 <richardwilliams23@yahoo.co.uk>
2022-07-08 04:00:26 +0000

    Temporary changes 2

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -9,17 +9,11 @@ from translator import english_to_french, french_to_english
 class TestEnglishToFrench(unittest.TestCase):
     """ Tests for the english_to_french function. """
     def test_english_to_french(self):
-        """ Test for null and one other """
-        self.assertNotEqual(english_to_french(''), '')
+        """ Tests for null and Hello to Bonjour """
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
 
 
-class TestFrenchToEnglish(unittest.TestCase):
-    """ Tests for the french_to_english function. """
-    def test_french_to_english(self):
-        """ Test for null and one other """
-        self.assertNotEqual(french_to_english(''), '')
-        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
 
 if __name__ == '__main__':
     unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -10,7 +10,7 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 
-url = os.environ['URL']
+url = os.environ['URL'] + '/v3/translate'
 apikey = os.environ['APIKEY']
 version = os.environ['VERSION']
 
@@ -26,24 +26,18 @@ language_translator.set_service_url( url )
 
 def english_to_french(english_text):
     """ Function to translate English text to French """
-    print("@@@ english_to_french: english_text is " + english_text)
+    print("########## english_to_french: english_text is " + english_text)
     # Retrieve the Detailed Response object.
     translation = language_translator.translate(
         text=english_text,
         model_id='fr-en').get_result()
     # Get translation as a string.
     french_text = translation['translations'][0]['translation']
+    print("########## english_to_french: french_text is " + french_text)
     return french_text
 
 
-def french_to_english(french_text):
-    print("@@@ french_to_english: french_text is " + french_text)
-    """ Function to translate French text to English """
-    # Retrieve the Detailed Response object.
-    translation_respose = language_translator.translate(
-        text=french_text,
-        model_id='en-fr')
-    translation = translation_respose.get_result()
-    # Get translation as a string.
-    english_text = translation['translations'][0]['translation']
-    return english_text
+print( '########## URL is ' + url )
+print( english_to_french('Hello') )
+
+
